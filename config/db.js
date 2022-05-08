333055d62d2a800b853b289cc2def7cb99b64b4d
marcos-mus <marcosmus2019@gmail.com>
2022-05-08 11:43:37 +0200

    feat : add db configuration

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default mysql.createConnection({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+});
